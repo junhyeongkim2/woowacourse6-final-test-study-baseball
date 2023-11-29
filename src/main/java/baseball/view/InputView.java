23d43df42d78d5e7941a79796e16f85313291ff8
jun <ggprgrkjh@naver.com>
2023-11-29 17:21:54 +0900

    refactor(inputview): 시작 재시작 번호 상수화

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -9,6 +9,9 @@ public class InputView {
     private static final String READ_RESTART_MESSAGE = "3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     private static final String INVALID_INPUT_ERROR_MESSAGE = "[ERROR] 잘못된 값이 입력되었습니다. 게임을 종료합니다.";
 
+    private static final String RESTART_NUMBER = "1";
+    private static final String END_NUMBER = "2";
+
 
     private static final String READNUMBER_FORM = "^\\d{3}+";
 
@@ -34,7 +37,7 @@ public class InputView {
 
 
     public static void validateReadRestartNumber(String input) {
-        if (!input.equals("1") && !input.equals("2")) {
+        if (!input.equals(RESTART_NUMBER) && !input.equals(END_NUMBER)) {
             throw new IllegalArgumentException(INVALID_INPUT_ERROR_MESSAGE);
         }
     }
