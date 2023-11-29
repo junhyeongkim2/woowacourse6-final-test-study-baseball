63e6044380035a1c89cd904382a7190d0d91642c
jun <ggprgrkjh@naver.com>
2023-11-29 15:21:17 +0900

    feat(baseballgamecontroller): 유저번호 string to List<Integer>로 변환 기능 추가

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -4,15 +4,28 @@ import baseball.model.Computer;
 import baseball.model.Generator;
 import baseball.view.InputView;
 import baseball.view.OutputView;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class BaseballGameController {
 
     void start() {
         OutputView.printStartMessage();
-        InputView.readNumbers();
         Computer computer = Computer.of(Generator.generateComputerNumbers());
 
+        do {
+            List<Integer> userNumbers = splitNumbers(InputView.readNumbers());
+            computer.calculateBall(userNumbers);
+            computer.calculateStrike(userNumbers);
+        } while (true);
 
+    }
+
+    public List<Integer> splitNumbers(String input) {
+        return Arrays.stream(input.split("")).map(userNumber -> Integer.parseInt(userNumber))
+                .collect(
+                        Collectors.toList());
     }
 
 
