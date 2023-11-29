12d2918516e1c6532cffc1dfe89a14007cd5eb28
jun <ggprgrkjh@naver.com>
2023-11-29 16:50:13 +0900

    test(inputview): 숫자 입력 세자리가 아닌 경우 예외 발생 테스트

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -1,6 +1,7 @@
 package baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
+import net.bytebuddy.pool.TypePool.Resolution.Illegal;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -8,5 +9,26 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class InputViewTest {
+
+    @DisplayName("숫자 입력 세자리가 아닌 경우 예외 발생 4자리 테스트")
+    @Test
+    void validateReadNumbersForm_IsNotThreeNumberFour_ExceptionThrow() {
+        assertThatThrownBy(() -> InputView.validateReadNumbersForm("1234")).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자 입력 세자리가 아닌 경우 예외 발생 2자리 테스트")
+    @Test
+    void validateReadNumbersForm_IsNotThreeNumberTwo_ExceptionThrow() {
+        assertThatThrownBy(() -> InputView.validateReadNumbersForm("12")).isInstanceOf(
+                IllegalArgumentException.class);
+    }
+
+    @DisplayName("숫자 입력 세자리가 아닌 경우 예외 발생 1자리 테스트")
+    @Test
+    void validateReadNumbersForm_IsNotThreeNumberOne_ExceptionThrow() {
+        assertThatThrownBy(() -> InputView.validateReadNumbersForm("1")).isInstanceOf(
+                IllegalArgumentException.class);
+    }
 
 }
