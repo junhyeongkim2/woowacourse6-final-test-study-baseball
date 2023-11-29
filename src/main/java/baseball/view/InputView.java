2ed9074881436ea1d0b96fc8275b626065ea81fd
jun <ggprgrkjh@naver.com>
2023-11-29 14:51:43 +0900

    feat(inputview): 숫자 입력 기능 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,4 +1,13 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+    public static String readNumbers() {
+        System.out.print("숫자를 입력해주세요 : ");
+        String input = Console.readLine();
+        return input;
+    }
+
 }
--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -1,11 +1,12 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 public class InputViewTest {
-
-
-
 
 }
