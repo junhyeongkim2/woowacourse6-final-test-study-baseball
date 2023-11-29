aa7e9f3b99dc860f57766004f2bb6496980b5080
jun <ggprgrkjh@naver.com>
2023-11-29 15:04:32 +0900

    feat(outputview): 컴퓨터는 입력한 숫자에 대한 결과 출력 기능 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -6,4 +6,38 @@ public class OutputView {
         System.out.println("숫자 야구 게임을 시작합니다.");
     }
 
+    public static void printResultMessage(int ballCount, int strikeCount) {
+        printOnlyBall(ballCount,strikeCount);
+        printOnlyStrike(ballCount,strikeCount);
+        printBallAndStrike(ballCount,strikeCount);
+        printNothing(ballCount,strikeCount);
+    }
+
+    public static void printOnlyBall(int ballCount, int strikeCount) {
+        if (ballCount != 0 && strikeCount == 0) {
+            System.out.println(ballCount + "볼");
+        }
+    }
+
+    public static void printOnlyStrike(int ballCount, int strikeCount) {
+        if (ballCount == 0 && strikeCount != 0) {
+            System.out.println(strikeCount + "스트라이크");
+        }
+    }
+
+    public static void printBallAndStrike(int ballCount, int strikeCount) {
+        if (ballCount != 0 && strikeCount != 0) {
+            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        }
+    }
+
+    public static void printNothing(int ballCount, int strikeCount){
+        if(ballCount==0 && strikeCount==0){
+            System.out.println("낫싱");
+        }
+    }
+
+
+
+
 }
--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -33,4 +33,63 @@ public class OutputViewTest {
         assertThat(output.toString()).isEqualTo("숫자 야구 게임을 시작합니다.\n");
 
     }
+
+    @DisplayName("1볼 1스트라이크 결과 메세지 출력 성공 테스트")
+    @Test
+    void printResultMessage_EqualOneBallOneStrikeMessage_Success() {
+        //given
+        ByteArrayOutputStream output = captureOutputValues();
+        int ballCount = 1;
+        int StrikeCount = 1;
+        //when
+        OutputView.printResultMessage(ballCount, StrikeCount);
+
+        //then
+        assertThat(output.toString()).isEqualTo("1볼 1스트라이크\n");
+
+    }
+
+    @DisplayName("2볼 0스트라이크 결과 메세지 출력 성공 테스트")
+    @Test
+    void printResultMessage_EqualTwoBallZeroStrikeMessage_Success() {
+        //given
+        ByteArrayOutputStream output = captureOutputValues();
+        int ballCount = 2;
+        int StrikeCount = 0;
+        //when
+        OutputView.printResultMessage(ballCount, StrikeCount);
+
+        //then
+        assertThat(output.toString()).isEqualTo("2볼\n");
+    }
+
+    @DisplayName("0볼 2스트라이크 결과 메세지 출력 성공 테스트")
+    @Test
+    void printResultMessage_EqualZeroBallTwoStrikeMessage_Success() {
+        //given
+        ByteArrayOutputStream output = captureOutputValues();
+        int ballCount = 0;
+        int StrikeCount = 2;
+        //when
+        OutputView.printResultMessage(ballCount, StrikeCount);
+
+        //then
+        assertThat(output.toString()).isEqualTo("2스트라이크\n");
+    }
+
+    @DisplayName("3스트라이크 결과 메세지 출력 성공 테스트")
+    @Test
+    void printResultMessage_EqualZeroBallThreeStrikeMessage_Success() {
+        //given
+        ByteArrayOutputStream output = captureOutputValues();
+        int ballCount = 0;
+        int StrikeCount = 3;
+        //when
+        OutputView.printResultMessage(ballCount, StrikeCount);
+
+        //then
+        assertThat(output.toString()).isEqualTo("3스트라이크\n");
+    }
+
+
 }
