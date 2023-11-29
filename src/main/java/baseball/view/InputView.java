4fb03813c98e6c77dee728af0ea5f3ec333427ae
jun <ggprgrkjh@naver.com>
2023-11-29 16:27:23 +0900

    feat(inputview): 재시작 입력에 숫자가 1 또는 2가 아닌 값이 들어오면 에러 발생 기능 추가

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -17,11 +17,19 @@ public class InputView {
     public static String readRestartNumber() {
         System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
         String input = Console.readLine();
+        validateReadRestartNumber(input);
         return input;
     }
 
     public static void validateReadNumbersForm(String input) {
         if (!Pattern.matches(READNUMBER_FORM, input)) {
+            throw new IllegalArgumentException("[ERROR] 잘못된 값이 입력되었습니다. 게임을 종료합니다.");
+        }
+    }
+
+
+    public static void validateReadRestartNumber(String input) {
+        if (!input.equals("1") && !input.equals("2")) {
             throw new IllegalArgumentException("[ERROR] 잘못된 값이 입력되었습니다. 게임을 종료합니다.");
         }
     }
