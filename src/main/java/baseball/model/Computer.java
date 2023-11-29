e52f9a001f6685ef96367a2176fa0db866f422a0
jun <ggprgrkjh@naver.com>
2023-11-29 14:32:26 +0900

    refactor(computer): 테스트 쉽게 하기 위해서 컴퓨터 넘버 외부에서 주입 가능 기능 추가

--- src/main/java/baseball/model/Computer.java
+++ src/main/java/baseball/model/Computer.java
@@ -6,14 +6,23 @@ import java.util.List;
 
 public class Computer {
 
+    private final List<Integer> numbers;
+
+    public Computer(List<Integer> numbers) {
+        this.numbers = numbers;
+    }
+
     public List<Integer> createNumbers() {
-        List<Integer> numbers = new ArrayList<>();
         while (numbers.size() < 3) {
             int randomNumber = Randoms.pickNumberInRange(1, 9);
             if (!numbers.contains(randomNumber)) {
                 numbers.add(randomNumber);
             }
         }
+        return numbers;
+    }
+
+    public List<Integer> getNumbers() {
         return numbers;
     }
 
--- src/test/java/baseball/model/ComputerTest.java
+++ src/test/java/baseball/model/ComputerTest.java
@@ -15,12 +15,27 @@ public class ComputerTest {
     @Test
     void createNumbers_EqualResult_Success() {
         //given
-        Computer computer = new Computer();
+        Computer computer = new Computer(List.of(1, 2, 3));
         //when
         List<Integer> numbers = computer.createNumbers();
         //then
         System.out.println(numbers);
         assertThat(numbers.size()).isEqualTo(3);
+        assertThat(computer.getNumbers().get(0)).isEqualTo(1);
+        assertThat(computer.getNumbers().get(1)).isEqualTo(2);
+        assertThat(computer.getNumbers().get(2)).isEqualTo(3);
+    }
+
+
+    @DisplayName("스트라이크 판별 성공 테스트")
+    @Test
+    void calculateStrike_EqualResult_Success() {
+        //given
+        Computer computer = new Computer(List.of(1, 2, 3));
+        //when
+        int strikeCount = computer.calculateStrike(List.of(1, 2, 4));
+        //then
+        assertThat(strikeCount).isEqualTo(2);
     }
 
 
