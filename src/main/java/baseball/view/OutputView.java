c477a36e0ff8bdd52cfb3079bc34259b12f634a7
jun <ggprgrkjh@naver.com>
2023-11-29 16:43:55 +0900

    refactor(outputview): 메세지 상수화 및 매직넘버 제거

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,41 +2,47 @@ package baseball.view;
 
 public class OutputView {
 
+    private static final String START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    private static final String BALL = "볼";
+    private static final String STRIKE = "스트라이크";
+    private static final String NOTHING = "낫싱";
+    private static final String SPACE = " ";
+    private static final int ZERO = 0;
+
     public static void printStartMessage() {
-        System.out.println("숫자 야구 게임을 시작합니다.");
+        System.out.println(START_MESSAGE);
     }
 
     public static void printResultMessage(int ballCount, int strikeCount) {
-        printOnlyBall(ballCount,strikeCount);
-        printOnlyStrike(ballCount,strikeCount);
-        printBallAndStrike(ballCount,strikeCount);
-        printNothing(ballCount,strikeCount);
+        printOnlyBall(ballCount, strikeCount);
+        printOnlyStrike(ballCount, strikeCount);
+        printBallAndStrike(ballCount, strikeCount);
+        printNothing(ballCount, strikeCount);
     }
 
     private static void printOnlyBall(int ballCount, int strikeCount) {
-        if (ballCount != 0 && strikeCount == 0) {
-            System.out.println(ballCount + "볼");
+        if (ballCount != ZERO && strikeCount == ZERO) {
+            System.out.println(ballCount + BALL);
         }
     }
 
     private static void printOnlyStrike(int ballCount, int strikeCount) {
-        if (ballCount == 0 && strikeCount != 0) {
-            System.out.println(strikeCount + "스트라이크");
+        if (ballCount == ZERO && strikeCount != ZERO) {
+            System.out.println(strikeCount + STRIKE);
         }
     }
 
     private static void printBallAndStrike(int ballCount, int strikeCount) {
-        if (ballCount != 0 && strikeCount != 0) {
-            System.out.println(ballCount + "볼 " + strikeCount + "스트라이크");
+        if (ballCount != ZERO && strikeCount != ZERO) {
+            System.out.println(ballCount + BALL+ SPACE + strikeCount + STRIKE);
         }
     }
 
-    private static void printNothing(int ballCount, int strikeCount){
-        if(ballCount==0 && strikeCount==0){
-            System.out.println("낫싱");
+    private static void printNothing(int ballCount, int strikeCount) {
+        if (ballCount == ZERO && strikeCount == ZERO) {
+            System.out.println(NOTHING);
         }
     }
-
 
 
 }
