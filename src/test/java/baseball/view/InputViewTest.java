ad89741e01db2d16ff8e2a4450824dbef5757286
jun <ggprgrkjh@naver.com>
2023-11-29 14:48:01 +0900

    feat(outputview): 숫자 야구 게임 시작 메세지를 출력 기능 추가

--- src/test/java/baseball/view/InputViewTest.java
+++ src/test/java/baseball/view/InputViewTest.java
@@ -1,4 +1,11 @@
 package baseball.view;
 
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
 public class InputViewTest {
+
+
+
+
 }
--- src/test/java/baseball/view/OutputViewTest.java
+++ src/test/java/baseball/view/OutputViewTest.java
@@ -1,4 +1,36 @@
 package baseball.view;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+
 public class OutputViewTest {
+
+    ByteArrayOutputStream captureOutputValues() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        PrintStream printStream = new PrintStream(outputStream);
+        System.setOut(printStream);
+        return outputStream;
+    }
+
+
+    @DisplayName("숫자 야구 시작 메세지 출력 성공 테스트")
+    @Test
+    void printStartMessage_EqualMessage_Success() {
+
+        //given
+        ByteArrayOutputStream output = captureOutputValues();
+
+        //when
+        OutputView.printStartMessage();
+
+        //then
+        assertThat(output.toString()).isEqualTo("숫자 야구 게임을 시작합니다.\n");
+
+    }
 }
